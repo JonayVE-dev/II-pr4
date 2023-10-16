eeb2b2cb7f2a89b5679d7ee637530edfc1466747
Gabriel Jonay Vera Estévez <alu0101398198@ull.edu.es>
2023-10-16 11:57:04 +0100

    Finish

--- scripts/ejer6/ejer6_notificador.cs
+++ scripts/ejer6/ejer6_notificador.cs
@@ -11,11 +11,13 @@ public class Notificador6 : MonoBehaviour
     {
         if (collision.gameObject.tag == "group1" && onIncreseScoreText != null) {
             onIncreseScoreText(5);
-            Destroy(collision.gameObject);
+            Vector3 newPostion = new Vector3(Random.Range(-10, 0), 0.5f, Random.Range(-10, -5));
+            collision.gameObject.transform.position = newPostion;
             transform.localScale += new Vector3(0.2f, 0.2f, 0.2f);
         } else if (collision.gameObject.tag == "group2" && onIncreseScoreText != null) {
             onIncreseScoreText(10);
-            Destroy(collision.gameObject);
+            Vector3 newPostion = new Vector3(Random.Range(-10, 0), 0.5f, Random.Range(-10, -5));
+            collision.gameObject.transform.position = newPostion;
             // aumentar tamaño del gameObject
             transform.localScale += new Vector3(0.4f, 0.4f, 0.4f);
         }
